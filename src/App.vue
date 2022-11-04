4416d822e8d2249ed38a8432e3bd6abd914214fa
Anatoliy Moystrenko <moistrenko@mail.ru>
2022-11-04 19:20:52 +0300

    create postList, postForm

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,79 @@
 <template>
-  <div>asdas</div>
+  <div class="app">
+    <post-form />
+
+    <post-list
+      v-for="post in posts"
+      :key="post.id"
+      :post="post"
+    />
+  </div>
 </template>
 
-<script></script>
+<script>
+import PostForm from './components/PostForm.vue';
+import PostList from './components/PostList.vue';
 
-<style scoped></style>
+export default {
+  data() {
+    return {
+      title: '',
+      body: '',
+      posts: [
+        {
+          id: 0,
+          title: 'zxcczcxc',
+          body: 'asd123e dadfvfvsdfcaxf vsxcvsd fdsaf',
+        },
+        {
+          id: 1,
+          title: 'zxcczcxc',
+          body: 'asd123e dadfvfvsdfcaxf vsxcvsd fdsaf',
+        },
+        {
+          id: 2,
+          title: 'zxcczcxc',
+          body: 'asd123e dadfvfvsdfcaxf vsxcvsd fdsaf',
+        },
+        {
+          id: 3,
+          title: 'zxcczcxc',
+          body: 'asd123e dadfvfvsdfcaxf vsxcvsd fdsaf',
+        },
+      ],
+    };
+  },
+
+  components: {
+    PostForm,
+    PostList,
+  },
+
+  methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      };
+
+      this.posts.push(newPost);
+
+      this.title = '';
+      this.body = '';
+    },
+  },
+};
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.app {
+  padding: 20px;
+}
+</style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,55 @@
+<template>
+  <form
+    class="form"
+    @submit.prevent
+  >
+    <h4>Создание поста</h4>
+    <input
+      v-model="title"
+      class="input"
+      type="text"
+      placeholder="Название"
+    />
+    <input
+      v-model="body"
+      class="input"
+      type="text"
+      placeholder="Описание"
+    />
+    <button
+      class="btn"
+      type="submit"
+      @click="createPost"
+    >
+      Создать
+    </button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="css" scoped>
+.form {
+  display: flex;
+  flex-direction: column;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+  cursor: pointer;
+}
+</style>
