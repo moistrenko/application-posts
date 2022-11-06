2b8cd829514bda5abf3c2a9d370d56bcad6f1e79
Anatoliy Moystrenko <moistrenko@mail.ru>
2022-11-06 11:17:18 +0300

    added transition

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -4,12 +4,15 @@
     class="post-list"
   >
     <h3>Список постов:</h3>
-    <post-item
-      v-for="post in posts"
-      :key="post.id"
-      :post="post"
-      @delete-post="this.$emit('delete-post', post)"
-    />
+
+    <transition-group name="post-list">
+      <post-item
+        v-for="post in posts"
+        :key="post.id"
+        :post="post"
+        @delete-post="this.$emit('delete-post', post)"
+      />
+    </transition-group>
   </div>
   <div v-else>
     <h3>Список постов пуст</h3>
@@ -28,3 +31,17 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.post-list-move,
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.5s ease;
+}
+
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(600px);
+}
+</style>
