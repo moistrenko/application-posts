327303a3a0ae58361bda5e40d778982974e19afc
Anatoliy Moystrenko <moistrenko@mail.ru>
2022-11-11 09:29:48 +0300

    update detail page

--- src/pages/PostDetailPage.vue
+++ src/pages/PostDetailPage.vue
@@ -1,9 +1,49 @@
 <template>
-  <div>Номер поста №{{ $route.params.id }}</div>
+  <div class="post-detail">
+    <div>ID: {{ post.id }}</div>
+    <h2>{{ post.title }}</h2>
+    <p>{{ post.body }}</p>
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      post: {},
+    };
+  },
+
+  mounted() {
+    this.getPost();
+  },
+
+  methods: {
+    async getPost() {
+      try {
+        await axios
+          .get(
+            `https://jsonplaceholder.typicode.com/posts/${this.$route.params.id}`,
+          )
+          .then((e) => e.data)
+          .then((data) => (this.post = data))
+          .catch((error) => {
+            console.error(error);
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="css" scoped>
+.post-detail {
+  width: 600px;
+  margin: 50px auto;
+  text-align: center;
+}
+</style>
